18ad939906bb3e1299898a6fba41c308184d9976
User18 <user18@user18de-imac.local>
2018-05-28 12:39:22 +0800

    調色APP

--- Color/ViewController.swift
+++ Color/ViewController.swift
@@ -14,7 +14,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var headBackGround: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var alphaLabel: UILabel!
+    
+    @IBAction func sliderColorChange(_ sender: UISlider){
+        headBackGround.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(alphaSlider.value)
+        )
+        if sender == redSlider{
+            redLabel.text = "\(redSlider.value)"
+        }else if sender  ==  greenSlider {
+            greenLabel.text  =  "\(greenSlider.value)"
+            
+        }else if sender  ==  blueSlider {
+            blueLabel.text  =  "\(blueSlider.value)"
+        }
+        else{
+            alphaLabel.text  =  "\(alphaSlider.value)"
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +50,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
